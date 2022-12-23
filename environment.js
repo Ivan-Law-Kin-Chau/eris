2916084184e059976fbfda240d1b9febc8e2ceee
Ivan Law Kin Chau <ivanlawkinchau@gmail.com>
2022-12-24 04:43:03 +0800

    Refactored the authorization logic

--- environment.js
+++ environment.js
@@ -10,6 +10,39 @@ module.exports = class Environment {
 		return true;
 	}
 	
+	authorize (client, message, listOfIds, callback, dungeonMasterMode = false) {
+		if (message.guild.id !== listOfIds.rootServer) {
+			message.reply(`Server not recognized! `);
+			return;
+		}
+		
+		let authorized = false;
+		
+		if (dungeonMasterMode === false) {
+			// Test if the message author is a President, Judge, Banker, Police Officer or Dungeon Master
+			Object.values(listOfIds.fiscalAuthorizers).forEach(role => {
+				message.guild.roles.cache.get(role).members.forEach(roleMember => {
+					if (roleMember.user.id === message.author.id) authorized = true;
+				});
+			});
+		} else if (dungeonMasterMode === true) {
+			// Test if the message author is a Dungeon Master
+			message.guild.roles.cache.get(listOfIds.fiscalAuthorizers.dungeonMaster).members.forEach(roleMember => {
+				if (roleMember.user.id === message.author.id) authorized = true;
+			});
+		} else {
+			message.reply(`Fatal error! `);
+		}
+		
+		if (authorized === false) {
+			message.reply(`Access denied! `);
+		} else if (authorized === true) {
+			callback();
+		} else {
+			message.reply(`Fatal error! `);
+		}
+	}
+	
 	evaluate (client, message, line, listOfIds) {
 		if (this.tokenIs(line[0], "help") && line.length === 1) {
 			message.reply(`Here is the list of available commands: \n**!e help** - See this message. \n**!e time** - See the current time in Discordia. \n**!e balance** - See your current balance. \n**!e budget** - See the government's current balance. \n**!e give {person} {number}** - Send your DCP to someone. \n**!e reward {person/role} {number} "{reason (optional)}"** - Send the government's DCP to people\\*. \n**!e charge {person/role} {number} "{reason (optional)}"** - Send people's DCP to the government\\*. \n**!e print {number}** - Print new DCP for the government\\*. \n**!e burn {number}** - Burn depreciated DCP held by the government\\*. \n**!e dice {probability of success (from 0 to 100 percent)} "{action}"** - Let fate decide whether an extrajudicial action is successful or not\\*\\*. \n\\* Only for Presidents, Judges, Bankers, Police Officers and Dungeon Masters. \n\\*\\* Only for Dungeon Masters. `);
@@ -56,151 +89,80 @@ module.exports = class Environment {
 				});
 			}
 		} else if (this.tokenIs(line[0], "reward") || this.tokenIs(line[0], "charge") && (line.length >= 3 || line.length <= 4)) {
-			if (message.guild.id !== listOfIds.rootServer) {
-				message.reply(`Server not recognized! `);
-				return;
-			}
-			
-			let authorized = false;
-			Object.values(listOfIds.fiscalAuthorizers).forEach(role => {
-				message.guild.roles.cache.get(role).members.forEach(roleMember => {
-					if (roleMember.user.id === message.author.id) authorized = true;
-				});
-			});
-			
-			if (authorized === false) {
-				message.reply(`Access denied! `);
-				return;
-			}
-			
-			if (!this.tokenIs(line[1], null, "account_id") && !this.tokenIs(line[1], null, "role_id")) {
-				message.reply(`Parameter 1 has to ping a user or a role! `);
-			} else if (!this.tokenIs(line[2], null, "integer")) {
-				message.reply(`Parameter 2 has to be a number! `);
-			} else if (line[2].content < 0) {
-				message.reply(`Parameter 2 cannot be less than 0! `);
-			} else {
-				const environment = this;
-				let rewardOrCharge = function (target, time, noReply = false) {
+			this.authorize(client, message, listOfIds, () => {
+				if (!this.tokenIs(line[1], null, "account_id") && !this.tokenIs(line[1], null, "role_id")) {
+					message.reply(`Parameter 1 has to ping a user or a role! `);
+				} else if (!this.tokenIs(line[2], null, "integer")) {
+					message.reply(`Parameter 2 has to be a number! `);
+				} else if (line[2].content < 0) {
+					message.reply(`Parameter 2 cannot be less than 0! `);
+				} else {
+					const environment = this;
 					if (environment.tokenIs(line[0], "reward")) {
 						environment.store.transact(function (value) {
 							if (value === null) {
 								message.reply(`Syntax error! `);
 							} else if (value === false) {
 								message.reply(`Bots cannot be rewarded! `);
-							} else if (value === true && noReply === false) {
+							} else if (value === true) {
 								message.reply(`They are rewarded successfully. `);
-								client.channels.cache.get(listOfIds.rewardChargeLogs).send(`<@${message.author.id}> has just rewarded <@${target}> ${line[2].content} DCP. `);
+								client.channels.cache.get(listOfIds.rewardChargeLogs).send(`<@${message.author.id}> has just rewarded <@${line[1].content}> ${line[2].content} DCP. `);
 								if (environment.tokenIs(line[3], null)) {
 									client.channels.cache.get(listOfIds.rewardChargeLogs).send(`**Reason provided: **${line[3].content}`);
 								}
 							}
-						}, time, line[2].content, "-1", target, message.author.id);
+						}, message.createdTimestamp, line[2].content, "-1", line[1].content, message.author.id);
 					} else if (environment.tokenIs(line[0], "charge")) {
 						environment.store.transact(function (value) {
 							if (value === null) {
 								message.reply(`Syntax error! `);
 							} else if (value === false) {
 								message.reply(`Bots cannot be charged! `);
-							} else if (value === true && noReply === false) {
+							} else if (value === true) {
 								message.reply(`They are charged successfully. `);
-								client.channels.cache.get(listOfIds.rewardChargeLogs).send(`<@${message.author.id}> has just charged <@${target}> ${line[2].content} DCP. `);
+								client.channels.cache.get(listOfIds.rewardChargeLogs).send(`<@${message.author.id}> has just charged <@${line[1].content}> ${line[2].content} DCP. `);
 								if (environment.tokenIs(line[3], null)) {
 									client.channels.cache.get(listOfIds.rewardChargeLogs).send(`**Reason provided: **${line[3].content}`);
 								}
 							}
-						}, time, line[2].content, target, "-1", message.author.id);
+						}, message.createdTimestamp, line[2].content, line[1].content, "-1", message.author.id);
 					}
 				}
-				
-				if (environment.tokenIs(line[1], null, "account_id")) {
-					rewardOrCharge(line[1].content, message.createdTimestamp);
-				} else if (environment.tokenIs(line[1], null, "role_id")) {
-					let list = [];
-					let time = message.createdTimestamp;
-					message.guild.roles.cache.get(line[1].content).members.forEach(roleMember => {
-						rewardOrCharge(roleMember.user.id, time, true);
-						list.push(roleMember.user.id);
-						time++;
-					});
-					
-					list = list.join(", ");
-					list = list.substring(0, list.length - 2);
-					if (environment.tokenIs(line[0], "reward")) {
-						message.reply(`Everyone has been rewarded successfully. `);
-						client.channels.cache.get(listOfIds.rewardChargeLogs).send(`<@${message.author.id}> has just rewarded all members of the <@&${line[1].content}> role ${line[2].content} DCP each. `);
-						if (environment.tokenIs(line[3], null)) {
-							client.channels.cache.get(listOfIds.rewardChargeLogs).send(`**Reason provided: **${line[3].content}`);
-						}
-					} else if (environment.tokenIs(line[0], "charge")) {
-						message.reply(`Everyone has been charged successfully. `);
-						client.channels.cache.get(listOfIds.rewardChargeLogs).send(`<@${message.author.id}> has just charged all members of the <@&${line[1].content}> role ${line[2].content} DCP each. `);
-						if (environment.tokenIs(line[3], null)) {
-							client.channels.cache.get(listOfIds.rewardChargeLogs).send(`**Reason provided: **${line[3].content}`);
-						}
-					}
-				}
-			}
-		} else if (this.tokenIs(line[0], "print") || this.tokenIs(line[0], "burn") && line.length === 2) {
-			if (message.guild.id !== listOfIds.rootServer) {
-				message.reply(`Server not recognized! `);
-				return;
-			}
-			
-			let authorized = false;
-			Object.values(listOfIds.fiscalAuthorizers).forEach(role => {
-				message.guild.roles.cache.get(role).members.forEach(roleMember => {
-					if (roleMember.user.id === message.author.id) authorized = true;
-				});
 			});
-			
-			if (authorized === false) {
-				message.reply(`Access denied! `);
-				return;
-			}
-			
-			if (!this.tokenIs(line[1], null, "integer")) {
-				message.reply(`Parameter 1 has to be a number! `);
-			} else if (line[1].content < 0) {
-				message.reply(`Parameter 1 cannot be less than 0! `);
-			} else {
-				if (this.tokenIs(line[0], "print")) {
-					this.store.transact(function (value) {
-						message.reply(`You have printed ${line[1].content} DCP. `);
-					}, message.createdTimestamp, line[1].content, "0", "-1", message.author.id);
-				} else if (this.tokenIs(line[0], "burn")) {
-					this.store.transact(function (value) {
-						message.reply(`You have burnt ${line[1].content} DCP. `);
-					}, message.createdTimestamp, line[1].content, "-1", "0", message.author.id);
-				}
-			}
-		} else if (this.tokenIs(line[0], "dice") && line.length === 3) {
-			if (message.guild.id === listOfIds.rootServer) {
-				let authorized = false;
-				message.guild.roles.cache.get(listOfIds.fiscalAuthorizers.dungeonMaster).members.forEach(roleMember => {
-					if (roleMember.user.id === message.author.id) authorized = true;
-				});
-				
-				if (authorized === false) {
-					message.reply(`Access denied! `);
-				} else if (authorized === true) {
-					if (!this.tokenIs(line[1], null, "integer")) {
-						message.reply(`Parameter 1 has to be a number! `);
-					} else if (line[1].content < 0) {
-						message.reply(`Parameter 1 cannot be less than 0! `);
-					} else if (line[1].content > 100) {
-						message.reply(`Parameter 1 cannot be more than 100! `);
-					} else if (!this.tokenIs(line[2], null)) {
-						message.reply(`Parameter 2 has to be a string! `);
-					} else if (Math.random() * 100 >= line[1].content) {
-						message.reply(`The attempt to ${line[2].content} is unsuccessful. `);
-					} else {
-						message.reply(`The attempt to ${line[2].content} is successful. `);
+		} else if (this.tokenIs(line[0], "print") || this.tokenIs(line[0], "burn") && line.length === 2) {
+			this.authorize(client, message, listOfIds, () => {
+				if (!this.tokenIs(line[1], null, "integer")) {
+					message.reply(`Parameter 1 has to be a number! `);
+				} else if (line[1].content < 0) {
+					message.reply(`Parameter 1 cannot be less than 0! `);
+				} else {
+					if (this.tokenIs(line[0], "print")) {
+						this.store.transact(function (value) {
+							message.reply(`You have printed ${line[1].content} DCP. `);
+						}, message.createdTimestamp, line[1].content, "0", "-1", message.author.id);
+					} else if (this.tokenIs(line[0], "burn")) {
+						this.store.transact(function (value) {
+							message.reply(`You have burnt ${line[1].content} DCP. `);
+						}, message.createdTimestamp, line[1].content, "-1", "0", message.author.id);
 					}
 				}
-			} else {
-				message.reply(`Server not recognized! `);
-			}
+			});
+		} else if (this.tokenIs(line[0], "dice") && line.length === 3) {
+			this.authorize(client, message, listOfIds, () => {
+				if (!this.tokenIs(line[1], null, "integer")) {
+					message.reply(`Parameter 1 has to be a number! `);
+				} else if (line[1].content < 0) {
+					message.reply(`Parameter 1 cannot be less than 0! `);
+				} else if (line[1].content > 100) {
+					message.reply(`Parameter 1 cannot be more than 100! `);
+				} else if (!this.tokenIs(line[2], null)) {
+					message.reply(`Parameter 2 has to be a string! `);
+				} else if (Math.random() * 100 >= line[1].content) {
+					message.reply(`The attempt to ${line[2].content} is unsuccessful. `);
+				} else {
+					message.reply(`The attempt to ${line[2].content} is successful. `);
+				}
+			}, true);
 		} else {
 			message.reply(`Command not recognized! `);
 		}
